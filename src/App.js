54a62529e86189792f2559783d640a47ecea7dee
shes50103 <shes50103@gmail.com>
2017-06-08 19:31:17 +0800

    a

--- src/App.js
+++ src/App.js
@@ -15,18 +15,28 @@ class App extends Component {
     console.log('AAA');
   }
 
+  gitList(){
+    var l = [1,2,3,4,5]
+    return l.map( i => <i> item{i} </i>);
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
+        <div className="App-header" ref="App-header" >
           <img src={logo} className="App-logo" alt="logo" />
-          <h2 onClick={this.handleClick}>Welcome to React</h2>
+          <h2 onClick={this.handleClick} > Welcome to React</h2>
 
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <Myc />
+       <div>
+          <ul>
+            {this.gitList()}
+          </ul>
+       </div>
         
       </div>
     );
